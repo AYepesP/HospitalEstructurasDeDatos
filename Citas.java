37ba01b1ada7baec8750631f3765689485219e49
AYepesP <alejandroyepespulido@gmail.com>
2023-02-21 18:14:50 -0500

    Se crearon getters y setters

--- Citas.java
+++ Citas.java
@@ -19,4 +19,77 @@ public class Citas {
         this.recordada = false;
     }
 
+
+    public Date getFecha() {
+        return this.fecha;
+    }
+
+    public void setFecha(Date fecha) {
+        this.fecha = fecha;
+    }
+
+    public Doctor getDoctor() {
+        return this.doctor;
+    }
+
+    public void setDoctor(Doctor doctor) {
+        this.doctor = doctor;
+    }
+
+    public Paciente getPaciente() {
+        return this.paciente;
+    }
+
+    public void setPaciente(Paciente paciente) {
+        this.paciente = paciente;
+    }
+
+    public String getSintomas() {
+        return this.sintomas;
+    }
+
+    public void setSintomas(String sintomas) {
+        this.sintomas = sintomas;
+    }
+
+    public String getDiagnostico() {
+        return this.diagnostico;
+    }
+
+    public void setDiagnostico(String diagnostico) {
+        this.diagnostico = diagnostico;
+    }
+
+    public Status getEstado() {
+        return this.estado;
+    }
+
+    public void setEstado(Status estado) {
+        this.estado = estado;
+    }
+
+    public boolean getRecordada() {
+        return this.recordada;
+    }
+
+    public void setRecordada(boolean recordada) {
+        this.recordada = recordada;
+    }
+
+    public String getSeguro() {
+        return this.seguro;
+    }
+
+    public void setSeguro(String seguro) {
+        this.seguro = seguro;
+    }
+
+    public String getNotasAdicionales() {
+        return this.notasAdicionales;
+    }
+
+    public void setNotasAdicionales(String notasAdicionales) {
+        this.notasAdicionales = notasAdicionales;
+    }
+
 }
